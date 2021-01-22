504eeb8db9104881c8fb2277cdced7a00ec3847f
Scott Sedgwick <ssedgwick@cochlear.com>
2021-01-22 14:40:07 +1100

    Initial commit

---
+++ CExample/CExample.cpp
@@ -0,0 +1,39 @@
+// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+
+static void updateID(uint8_t* idstr, uint32_t id)
+{
+	int n;
+	idstr[0] = 7;
+	for (n = 2; n < 9; n++) {
+		idstr[n] = 0x30 + (id & 0x1f);
+		id >>= 5;  //Right shifts bits of id by 5 and stores the result in id.
+	}
+}
+
+int main()
+{
+	uint32_t hsh = 0x474E5253L;
+	uint8_t result[9] = { 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20 };  // Initialize with spaces
+	uint32_t ownId = 0x95C6DA80;
+
+	printf("Initial Hash : 0x%x\n", hsh);
+	printf("Initial OwnID: 0x%x\n", ownId);
+
+	updateID(result, hsh ^ ownId);  // ^ is bitwise XOR
+
+    std::cout << "Updated ID:\n";
+
+	int n;
+	for (n = 0; n < 9; n++) {
+		printf("0x%x ", result[n]);
+	}
+
+	std::cout << "\n\nPress Enter to continue.\n";
+	std::string buf;
+	std::getline(std::cin, buf);
+	return 0;
+}
---
+++ CSharpExample/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace CSharpExample
+{
+    class Program
+    {
+        static byte[] updateID(uint id)
+        {
+            int n;
+            byte[] result = { 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20 };  // Initialize with spaces
+            result[0] = 7;
+            for (n = 2; n < 9; n++)
+            {
+                result[n] = (byte)(0x30 + (id & 0x1f));
+                id >>= 5;  //Right shifts bits of id by 5 and stores the result in id.
+            }
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            uint hsh = 0x474E5253;
+            byte[] result;
+            uint ownId = 0x95C6DA80;
+
+            Console.WriteLine($"Initial Hash : 0x{hsh}");
+            Console.WriteLine($"Initial OwnID: 0x{ownId}");
+
+            result = updateID(hsh ^ ownId);  // ^ is bitwise XOR
+
+            Console.WriteLine("Updated ID:");
+
+            int n;
+            for (n = 0; n < 9; n++)
+            {
+                Console.Write($"0x{result[n]:x} ");
+            }
+
+            Console.WriteLine("\n\nPress Enter to continue.");
+            Console.ReadLine();
+        }
+    }
+}
